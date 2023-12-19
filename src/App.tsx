b7e6b4fce9277d9bce62969c06eb95876f6888b8
Reinaldo Santos <kingnaldoreinaldo@gmail.com>
2023-12-19 11:51:39 -0300

    feat: calling the component screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Nav } from './components/nav/nav';
+import { Screen } from './components/screen/screen';
 export default function App(): React.ReactElement {
 	return (
-		<section className='bg-imgBg bg-cover bg-no-repeat bg-center w-screen h-screen overflow-hidden overflow-x-hidden'>
+		<Screen>
 			<Nav selected={1} />
-		</section>
+			<div className='bg-red-500 w-full h-full'></div>
+		</Screen>
 	);
 }
