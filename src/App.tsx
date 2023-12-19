ef85ef30867473992da8f3ae71fcbf024b3a3b4e
Reinaldo Santos <kingnaldoreinaldo@gmail.com>
2023-12-19 18:19:47 -0300

    feat: added RouterProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import { Nav } from './components/nav/nav';
-import { Screen } from './components/screen/screen';
+
+import { RouterProvider } from 'react-router-dom';
+import { router } from './routes/routes';
 export default function App(): React.ReactElement {
 	return (
-		<Screen>
-			<Nav selected={1} />
-			<div className='bg-red-500 w-full h-full'></div>
-		</Screen>
+		<RouterProvider router={router} />
 	);
 }
