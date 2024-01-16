3f6f5a632222f0d93cf5392c062bd6f494356dfe
Reinaldo-santos-cerquira <kingnaldoreinaldo@gmail.com>
2024-01-16 19:23:01 -0300

    chore: add paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	resolve: {
+		alias: {
+			'@components': '/src/components',
+			'@hooks': '/src/hooks',
+			'@pages': '/src/pages',
+			'@utils': '/src/utils',
+			'@routes': '/src/routes',
+			'@domain': '/src/domain',
+		},
+	},
+	plugins: [react()],
+});
