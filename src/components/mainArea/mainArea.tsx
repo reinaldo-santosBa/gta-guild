152d0a87667a7744432be8810009f11f40f9ea6e
Reinaldo-santos-cerquira <kingnaldoreinaldo@gmail.com>
2024-01-17 18:29:38 -0300

    feat: isolation infoarea

--- src/components/mainArea/mainArea.tsx
+++ src/components/mainArea/mainArea.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
-import { BtnAreaHome, HomeAreaAlert, NewsPenaltiesArea } from '@components';
+import { InfoArea, ListMember } from '@components';
 
 export const MainArea: React.FC = () => {
 	return (
 		<div className='flex flex-col w-full p-[1.25rem]'>
-			<div className='flex flex-col gap-3 flex-[4] w-full'>
-				<HomeAreaAlert />
-				<NewsPenaltiesArea />
-				<BtnAreaHome />
-			</div>
-			<div className='flex flex-[6] w-full'></div>
+			<InfoArea />
+			<ListMember />
 		</div>
 	);
 };
