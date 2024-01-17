4843af2ce0c4aca5002dbf75a2026c35bc5a5aed
Reinaldo-santos-cerquira <kingnaldoreinaldo@gmail.com>
2024-01-17 15:07:21 -0300

    feat: export main area

--- src/components/index.ts
+++ src/components/index.ts
@@ -6,3 +6,4 @@ export * from './newsArea/newsArea';
 export * from './notifyAlert/notifyAlert';
 export * from './screen/screen';
 export * from './title18px/title18px';
+export * from './mainArea/mainArea';
