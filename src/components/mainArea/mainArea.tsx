35088b5df3a071a8793dc85ed0eeb02bdbeaed94
Reinaldo-santos-cerquira <kingnaldoreinaldo@gmail.com>
2024-01-18 16:46:04 -0300

    feat: added component ListMenber and HeaderListMember

--- src/components/mainArea/mainArea.tsx
+++ src/components/mainArea/mainArea.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { InfoArea, ListMember } from '@components';
+import { HeaderListMember, InfoArea, ListMember } from '@components';
 
 export const MainArea: React.FC = () => {
 	return (
 		<div className='flex flex-col w-full p-[1.25rem]'>
 			<InfoArea />
+			<HeaderListMember />
 			<ListMember />
 		</div>
 	);
