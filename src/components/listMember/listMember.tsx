041bec83d13b006676f9d643c9ca8a677f1398cd
Reinaldo-santos-cerquira <kingnaldoreinaldo@gmail.com>
2024-01-29 18:10:03 -0300

    feat: changed 3 buttontable

--- src/components/listMember/listMember.tsx
+++ src/components/listMember/listMember.tsx
@@ -1,5 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { IMember, memberService } from '@domain';
+import { v4 } from 'uuid';
+import { IoIosArrowDown, IoIosArrowUp } from 'react-icons/io';
+import { ImExit } from 'react-icons/im';
+
+import { ButtonTable } from '../buttonTable/buttonTable';
 export const ListMember: React.FC = () => {
 	const [memberList, setMemberList] = useState<IMember[]>([]);
 	useEffect(() => {
@@ -10,12 +15,16 @@ export const ListMember: React.FC = () => {
 			}
 		)();
 	}, []);
+	const promotion = () => {
+		console.log('p');
+
+	};
 	return (
 		<div className='flex flex-col flex-[6] w-full mt-4 max-h-full overflow-auto'>
 			<div className='w-full h-full py-4'>
 				{
 					memberList.map((member) => (
-						<div key={member.id} className='flex'>
+						<div key={v4()} className='flex mb-4'>
 							<div className='flex flex-1 justify-center items-center text-white'>
 								<p>{member.id}</p>
 							</div>
@@ -34,7 +43,26 @@ export const ListMember: React.FC = () => {
 							<div className='flex flex-1 justify-center items-center text-white'>
 								<p>{member.timeOfOrg}</p>
 							</div>
-							<div className='flex flex-[3] justify-center items-center text-white'></div>
+							<div className='flex flex-[3] gap-4 justify-center items-center text-white'>
+								<ButtonTable
+									icon={<IoIosArrowUp />}
+									text={'Promover'}
+									bg={'blue'}
+									onClick={promotion}
+								/>
+								<ButtonTable
+									icon={<IoIosArrowDown />}
+									text={'Rebaixar'}
+									bg={'blue'}
+									onClick={promotion}
+								/>
+								<ButtonTable
+									icon={<ImExit />}
+									text={'Demitir'}
+									bg={'red'}
+									onClick={promotion}
+								/>
+							</div>
 						</div>
 					))
 				}
