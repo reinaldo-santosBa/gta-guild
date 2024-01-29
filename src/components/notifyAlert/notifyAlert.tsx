c9aaae9a195e90f8983de36802e1ea1edda59e91
Reinaldo-santos-cerquira <kingnaldoreinaldo@gmail.com>
2024-01-29 18:11:15 -0300

    feat: added min height

--- src/components/notifyAlert/notifyAlert.tsx
+++ src/components/notifyAlert/notifyAlert.tsx
@@ -12,7 +12,7 @@ export const NotifyAlert: React.FC<NotifyAlertTypes> = ({ title, message, border
 	}
 
 	return (
-		<div className={`flex-none py-[.75rem] px-[.85rem] bg-bgContrast  w-full h-[100%] rounded-[.5rem] border-b-2 ${borderColors}`}>
+		<div className={`flex-none py-[.75rem] px-[.85rem] bg-bgContrast  w-full h-min rounded-[.5rem] border-b-2 ${borderColors}`}>
 			<p className='text-white font-bold text-[1rem]'>{title}</p>
 			<p className='text-subTitle text-[.75rem] font-normal'>{message.length > 40 ? message.slice(0, 90) + '...' : message}</p>
 		</div>
